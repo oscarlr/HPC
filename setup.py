97812868942292f1f94bb7e7f1566b920259f135
Oscar Rodriguez <olrodriguez01@gmail.com>
2016-06-03 17:48:19 -0400

    Adding model

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='HPC',
+    url='https://github.com/oscarlr/HPC',
+    description='Small class to submit jobs into Minerva using Harms submitjob',
+    packages=['HPC'],
+    include_package_data=True,
+    platforms='any'
+)
